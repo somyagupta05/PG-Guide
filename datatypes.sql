4c6c10e7616fd390a3e37cdeb0a92258f3b68813
somyagupta05 <somyagupta052003@gmail.com>
2025-01-29 00:18:44 +0530

    pg

--- datatypes.sql
+++ datatypes.sql
@@ -12,3 +12,17 @@
 --the primary key constraints uniquely identifies each record in a table
 --primary keys must contain unique values and cannot contain null values
 --a table can have only one primary key
+ -----
+ --NOT NULL
+--null values are not allowed in this particular column
+
+create table customers(id int not null, name varchar(100) not null) ------
+----
+--DEFAULT VALUE
+-- will not give place to null but write savings
+
+create table customers(acc_no int primary key, name varchar(100) not null, acc_type varchar(50) not null default 'savings') ----
+--AUTO INCREMENT
+-- will automatically write id
+
+create table employees(id serial primary key, firstname varchar(50), lastname varchar(50));
