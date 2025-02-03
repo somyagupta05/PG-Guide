787017430a95e2ba7bfb0d513d95ec5294789645
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:10:11 +0530

    clause

--- clauses.sql
+++ clauses.sql
@@ -8,4 +8,8 @@
  -- where
 bank=# select * from employees where emp_id=5;
  emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+------------------------+------+----------+------------
- 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 (1 row)
+ 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 (1 row);
+
+bank=# select * from employees where dept='HR';
+ emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+-------------------------+------+----------+------------
+ 2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 (2 rows)
