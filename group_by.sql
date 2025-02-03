4eb145638207c2a96cc099f6050e98adaca86634
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:43:13 +0530

    Group by

--- group_by.sql
+++ group_by.sql
@@ -1,2 +1,9 @@
 -- use case
 -- No of employees in each dept
+ bank=# select dept from employees group by dept;
+ dept -----------
+ Marketing Finance IT HR (4 rows) ;
+
+bank=# select dept, count(emp_id) from employees group by dept;
+ dept | count -----------+-------
+ Marketing | 2 Finance | 2 IT | 4 HR | 2 (4 rows);
