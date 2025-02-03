0ad84e95c7efbd17d63e67a9ae2cac587648dc8a
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:43:48 +0530

    Group by

--- group_by.sql
+++ group_by.sql
@@ -7,3 +7,8 @@
 bank=# select dept, count(emp_id) from employees group by dept;
  dept | count -----------+-------
  Marketing | 2 Finance | 2 IT | 4 HR | 2 (4 rows);
+
+--  salary
+bank=# select dept, sum(salary) from employees group by dept;
+ dept | sum -----------+-----------
+ Marketing | 102000.00 Finance | 121000.00 IT | 206000.00 HR | 92000.00 (4 rows) ;
