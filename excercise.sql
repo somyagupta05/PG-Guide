15b89fc331f2644e79683ba0c2dea9cd5aa301e1
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 00:17:52 +0530

    excersise

--- excercise.sql
+++ excercise.sql
@@ -22,3 +22,15 @@ bank=# select concat_ws(':',emp_id,fname,upper(dept)) from employees where emp_i
 bank=# select concat(left(dept,1),emp_id),fname from employees;
  concat | fname --------+--------
  I1 | Raj H2 | Priya I3 | Arjun F4 | Suman H5 | Kavita M6 | Amit I7 | Neha I8 | Rahul F9 | Anjali M10 | Vijay;
+
+-- ............................................
+ -- QUESTIONS
+-- 1. FIND DIFFERENT TYPE OF DEPARTMENT IN DATABSES?
+bank=# SELECT DISTINCT(DEPT) FROM EMPLOYEES;
+ dept -----------
+ Marketing Finance IT HR (4 rows);
+
+-- 2. DISPLAY RECORDS WITH HIGH-LOW SALARY
+ -- 3.HOW TO SEE ONLY TOP3 RECORDS FROM A TABLE?
+ -- 4.SHOW RECORDS WHERE FIRST NAME START WITH LETTER 'A'?
+ -- 5.SHOW RECORDS WHERE LENGTH OF THE LNAME IS 4 CHARACTER?
