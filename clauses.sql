f8bb657ee36e500055c963e4c0bc7fda2e7b449f
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:12:26 +0530

    clause

--- clauses.sql
+++ clauses.sql
@@ -14,6 +14,12 @@ bank=# select * from employees where dept='HR';
  emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+-------------------------+------+----------+------------
  2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 (2 rows);
 
+-- OR.......................
 bank=# select * from employees where dept='HR' or dept='Finance';
  emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+--------------------------+---------+----------+------------
- 2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 4 | Suman | Patel | suman.patel@example.com | Finance | 60000.00 | 2018-07-30 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 9 | Anjali | Mehta | anjali.mehta@example.com | Finance | 61000.00 | 2018-12-03 (4 rows)
+ 2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 4 | Suman | Patel | suman.patel@example.com | Finance | 60000.00 | 2018-07-30 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 9 | Anjali | Mehta | anjali.mehta@example.com | Finance | 61000.00 | 2018-12-03 (4 rows);
+
+--  AND............
+bank=# select * from employees where dept='IT' and salary>50000;
+ emp_id | fname | lname | email | dept | salary | hire_date --------+-------+-------+-------------------------+------+----------+------------
+ 3 | Arjun | Verma | arjun.verma@example.com | IT | 55000.00 | 2021-06-01 8 | Rahul | Kumar | rahul.kumar@example.com | IT | 53000.00 | 2021-02-14 (2 rows)
