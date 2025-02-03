3c391f433351718b956782ca0a5cf998eeaad9cd
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 00:14:54 +0530

    excersise

--- excercise.sql
+++ excercise.sql
@@ -17,3 +17,8 @@ concat_ws -----------------
 bank=# select concat_ws(':',emp_id,fname,upper(dept)) from employees where emp_id=4;
  concat_ws -----------------
  4:Suman:FINANCE (1 row);
+
+--  TASK 4 FIRST LETTER OF DEPT AND THEN EMP_ID
+bank=# select concat(left(dept,1),emp_id),fname from employees;
+ concat | fname --------+--------
+ I1 | Raj H2 | Priya I3 | Arjun F4 | Suman H5 | Kavita M6 | Amit I7 | Neha I8 | Rahul F9 | Anjali M10 | Vijay;
