260b2a1b4a7ed29e08d103d3ed81b4d88f3972ce
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:08:46 +0530

    basics

--- clauses.sql
+++ clauses.sql
@@ -4,3 +4,8 @@
 -- order by
 -- limit
 -- like
+ -- ..............................
+ -- where
+bank=# select * from employees where emp_id=5;
+ emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+------------------------+------+----------+------------
+ 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 (1 row)
