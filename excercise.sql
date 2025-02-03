f56672194a9ad6d2f1cbaa6fcf8066da62106b75
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 00:12:30 +0530

    excersise

--- excercise.sql
+++ excercise.sql
@@ -4,3 +4,16 @@ p_id=1;
 
 concat_ws -----------------
  1:Raj:Sharma:IT (1 row);
+
+--  TASK TWO SPACE IN RAJ SHARMA
+bank=# select concat_ws(':',emp_id,concat_ws(' ',fname,lname),dept) from emp
+loyees
+where emp_id=1;
+
+concat_ws -----------------
+ 1:Raj Sharma:IT (1 row);
+
+--  TASK THREE DEPT IN UPPER CASE
+bank=# select concat_ws(':',emp_id,fname,upper(dept)) from employees where emp_id=4;
+ concat_ws -----------------
+ 4:Suman:FINANCE (1 row);
