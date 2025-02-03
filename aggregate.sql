448aa3b15612379771d8b7d8eed3cceb948432ef
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:34:18 +0530

    Aggregate function- count

--- aggregate.sql
+++ aggregate.sql
@@ -2,3 +2,13 @@
 -- how to find total number of employees?
 -- employees with max salary?
 -- Average salary of employees
+-- count
+-- sum
+-- avg
+-- min
+-- max
+-- .......................
+-- COUNT
+bank=# select count(emp_id) from employees;
+ count -------
+ 10 (1 row);
