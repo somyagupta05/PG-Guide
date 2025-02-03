ebe51b2b8c319625b7c8e02d90e71c99b910b441
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:16:44 +0530

    IN  Opeartaor

--- clauses.sql
+++ clauses.sql
@@ -37,3 +37,9 @@ bank=# select * from employees where dept in ('IT','HR','Finance');
  emp_id | fname | lname | email | dept | salary | hire_date --------+--------+--------+--------------------------+---------+----------+------------
  1 | Raj | Sharma | raj.sharma@example.com | IT | 50000.00 | 2020-01-15 2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 3 | Arjun | Verma | arjun.verma@example.com | IT | 55000.00 | 2021-06-01 4 | Suman | Patel | suman.patel@example.com | Finance | 60000.00 | 2018-07-30 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 7 | Neha | Desai | neha.desai@example.com | IT | 48000.00 | 2019-05-18 8 | Rahul | Kumar | rahul.kumar@example.com | IT | 53000.00 | 2021-02-14 9 | Anjali | Mehta | anjali.mehta@example.com | Finance | 61000.00 | 2018-12-03 (8 rows);
 
+-- ....................................
+-- NOT IN OPERTAOR
+bank=# select * from employees where dept not in ('IT','HR','Finance');
+ emp_id | fname | lname | email | dept | salary | hire_date --------+-------+-------+------------------------+-----------+----------+------------
+ 6 | Amit | Gupta | amit.gupta@example.com | Marketing | 52000.00 | 2020-09-25 10 | Vijay | Nair | vijay.nair@example.com | Marketing | 50000.00 | 2020-04-19 (2 rows) ;
+
