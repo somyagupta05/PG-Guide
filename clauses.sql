24cc43db1da05e8705df5bb6ed548bac2363d0fc
somyagupta05 <somyagupta052003@gmail.com>
2025-02-03 23:29:28 +0530

    Like clause

--- clauses.sql
+++ clauses.sql
@@ -107,3 +107,8 @@ bank=# select * from employees where fname like '%a';
 bank=# select * from employees where fname like '%i%';
  emp_id | fname | lname | email | dept | salary | hire_date --------+--------+-------+--------------------------+-----------+----------+------------
  2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 6 | Amit | Gupta | amit.gupta@example.com | Marketing | 52000.00 | 2020-09-25 9 | Anjali | Mehta | anjali.mehta@example.com | Finance | 61000.00 | 2018-12-03 10 | Vijay | Nair | vijay.nair@example.com | Marketing | 50000.00 | 2020-04-19 (5 rows) ;
+
+--  when there is only 2 letters in dept
+bank=# select * from employees where dept like '__';
+ emp_id | fname | lname | email | dept | salary | hire_date --------+--------+--------+-------------------------+------+----------+------------
+ 1 | Raj | Sharma | raj.sharma@example.com | IT | 50000.00 | 2020-01-15 2 | Priya | Singh | priya.singh@example.com | HR | 45000.00 | 2019-03-22 3 | Arjun | Verma | arjun.verma@example.com | IT | 55000.00 | 2021-06-01 5 | Kavita | Rao | kavita.rao@example.com | HR | 47000.00 | 2020-11-10 7 | Neha | Desai | neha.desai@example.com | IT | 48000.00 | 2019-05-18 8 | Rahul | Kumar | rahul.kumar@example.com | IT | 53000.00 | 2021-02-14 (6 rows) ;
