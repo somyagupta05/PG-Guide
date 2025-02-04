9993a433508d1f46d901e9dbbe5d8498207209a8
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 16:00:10 +0530

    altering

--- altering.sql
+++ altering.sql
@@ -12,3 +12,10 @@ postgres=# alter table person drop column age;
 ALTER TABLE postgres=# select * from person;
  id | name | city -----+------+----------
  102 | Sham | Mumbai 103 | Paul | Chennai 101 | Raju | Banglore (3 rows);
+
+--  add default value as 0 in age
+postgres=# alter table person add column age int default 0;
+
+ALTER TABLE postgres=# select * from person;
+ id | name | city | age -----+------+----------+-----
+ 102 | Sham | Mumbai | 0 103 | Paul | Chennai | 0 101 | Raju | Banglore | 0 (3 rows) ;
