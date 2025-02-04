bee2b3c94e6938ec2f3261ea121e9c00ff1a033e
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 16:25:21 +0530

    check

--- check.sql
+++ check.sql
@@ -31,3 +31,15 @@ ALTER TABLE postgres=# insert into person(mob) values (123);
 INSERT 0 1 postgres=# select * from person;
  id | fname | city | age | mob -----+-------+----------+-----+------------
  102 | Sham | Mumbai | 0 | 103 | Paul | Chennai | 0 | 101 | Raju | Banglore | 0 | | | | 0 | 1234567890 | | | 0 | 123 (5 rows);
+
+--will give this as a readable error;
+ postgres=# alter table person add constraint mob_no_less_than_10 check (length(mob)>=10);
+ERROR: constraint "mob_no_less_than_10"
+for relation "person" already exists postgres=# insert into person(mob) values (123);
+ERROR: new row
+for relation "person" violates check constraint "mob_no_less_than_10" DETAIL: Failing row contains (null,
+                                                                                                    null,
+                                                                                                    null,
+                                                                                                    0,
+                                                                                                    123).;
+
