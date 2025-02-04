51f46dbc92e56cfb5f93f43e28326ec7e5e74702
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 15:58:03 +0530

    altering

--- altering.sql
+++ altering.sql
@@ -4,4 +4,11 @@ postgres=# alter table person add column age int;
 
 ALTER TABLE postgres=# select * from person;
  id | name | city | age -----+------+----------+-----
- 102 | Sham | Mumbai | 103 | Paul | Chennai | 101 | Raju | Banglore | (3 rows)
+ 102 | Sham | Mumbai | 103 | Paul | Chennai | 101 | Raju | Banglore | (3 rows);
+
+--  drop column age
+postgres=# alter table person drop column age;
+
+ALTER TABLE postgres=# select * from person;
+ id | name | city -----+------+----------
+ 102 | Sham | Mumbai 103 | Paul | Chennai 101 | Raju | Banglore (3 rows);
