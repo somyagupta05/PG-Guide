9436557753f11e1fa523cb9f68b517811d0a3f51
somyagupta05 <somyagupta052003@gmail.com>
2025-02-04 16:45:20 +0530

    relationships

---
+++ Realtionships.sql
@@ -0,0 +1,9 @@
+-- when something is connected
+-- types of relationship
+-- 1. one to one
+-- 2. one to many
+-- 3. many to many
+ -- 1. ONE TO ONE --there are two table employees and emplyeeid and there is person name raju if his detail comes once in both the table then its one to one
+ -- 2. ONR TO MANY -- two tables employees and employee task raju is once in employees but in employee task raju's task are more than one so he has come more than one time its one to many.
+--
+ --  3.MANY TO MANY --if a book has more than one author and one author have written more than one book then its many to many
