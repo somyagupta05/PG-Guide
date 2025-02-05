509122be57eec55f7c0e2d5b092fb4604e766b01
somyagupta05 <somyagupta052003@gmail.com>
2025-02-06 00:08:40 +0530

    many-many

--- project_e-store.sql
+++ project_e-store.sql
@@ -1,57 +1,65 @@
-CREATE TABLE customers (
-    cust_id SERIAL PRIMARY KEY,
-    cust_name VARCHAR(100) NOT NULL
-);
+CREATE TABLE customers ( cust_id SERIAL PRIMARY KEY,
+                                                cust_name VARCHAR(100) NOT NULL);
 
 
-CREATE TABLE orders (
-    ord_id SERIAL PRIMARY KEY,
-    ord_date DATE NOT NULL,
-    cust_id INTEGER NOT NULL,
-    FOREIGN KEY (cust_id) REFERENCES customers(cust_id)
-);
+CREATE TABLE orders ( ord_id SERIAL PRIMARY KEY,
+                                            ord_date DATE NOT NULL,
+                                                          cust_id INTEGER NOT NULL,
+                     FOREIGN KEY (cust_id) REFERENCES customers(cust_id));
 
 
-CREATE TABLE order_items (
-    item_id SERIAL PRIMARY KEY,
-    ord_id INTEGER NOT NULL,
-    p_id INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    FOREIGN KEY (ord_id) REFERENCES orders(ord_id),
-    FOREIGN KEY (p_id) REFERENCES products(p_id)
-);
+CREATE TABLE order_items ( item_id SERIAL PRIMARY KEY,
+                                                  ord_id INTEGER NOT NULL,
+                                                                 p_id INTEGER NOT NULL,
+                                                                              quantity INTEGER NOT NULL,
+                          FOREIGN KEY (ord_id) REFERENCES orders(ord_id),
+                          FOREIGN KEY (p_id) REFERENCES products(p_id));
 
 
-CREATE TABLE products (
-    p_id SERIAL PRIMARY KEY,
-    p_name VARCHAR(100) NOT NULL,
-    price NUMERIC NOT NULL
-);
+CREATE TABLE products ( p_id SERIAL PRIMARY KEY,
+                                            p_name VARCHAR(100) NOT NULL,
+                                                                price NUMERIC NOT NULL);
+
 
 INSERT INTO customers (cust_name)
-VALUES
-    ('Raju'), ('Sham'), ('Paul'), ('Alex');
+VALUES ('Raju'), ('Sham'), ('Paul'), ('Alex');
+
 
 INSERT INTO orders (ord_date, cust_id)
-VALUES
-    ('2024-01-01', 1),  -- Raju first order
-    ('2024-02-01', 2),  -- Sham first order
-    ('2024-03-01', 3),  -- Paul first order
-    ('2024-04-04', 2);  -- Sham second order
+VALUES ('2024-01-01',
+        1), -- Raju first order
+ ('2024-02-01',
+  2), -- Sham first order
+ ('2024-03-01',
+  3), -- Paul first order
+ ('2024-04-04',
+  2); -- Sham second order
+
 
 INSERT INTO order_items (ord_id, p_id, quantity)
-VALUES
-    (1, 1, 1),  -- Raju ordered 1 Laptop
-    (1, 4, 2),  -- Raju ordered 2 Cables
-    (2, 1, 1),  -- Sham ordered 1 Laptop
-    (3, 2, 1),  -- Paul ordered 1 Mouse
-    (3, 4, 5),  -- Paul ordered 5 Cables
-    (4, 3, 1);  -- Sham ordered 1 Keyboard
+VALUES (1,
+        1,
+        1), -- Raju ordered 1 Laptop
+ (1,
+  4,
+  2), -- Raju ordered 2 Cables
+ (2,
+  1,
+  1), -- Sham ordered 1 Laptop
+ (3,
+  2,
+  1), -- Paul ordered 1 Mouse
+ (3,
+  4,
+  5), -- Paul ordered 5 Cables
+ (4,
+  3,
+  1); -- Sham ordered 1 Keyboard
+
 
 INSERT INTO products (p_name, price)
-VALUES
-    ('Laptop', 55000.00),
-    ('Mouse', 500),
-    ('Keyboard', 800.00),
-    ('Cable', 250.00)
-;	
+VALUES ('Laptop',
+        55000.00), ('Mouse',
+                    500), ('Keyboard',
+                           800.00), ('Cable',
+                                     250.00) ;
