3be4a118f5229bf1c325373f2073737ba6f2d906
somyagupta05 <somyagupta052003@gmail.com>
2025-02-19 15:07:00 +0530

    Triggers

--- Triggers.sql
+++ Triggers.sql
@@ -1,1 +1,1 @@
--- TRIGGERS are special procedures in a database that automatically execute predefined actions in respinse to certain events on a specified table or view.
+-- TRIGGERS are special procedures in a database that automatically execute predefined actions in response to certain events on a specified table or view.
