e244e6921e0e0fbeb7a23f43b4ef21d536629a55
somyagupta05 <somyagupta052003@gmail.com>
2025-02-19 09:05:09 +0530

    CTE use case 1

--- CTE.sql
+++ CTE.sql
@@ -2,3 +2,19 @@
 -- .................................................
  -- use case 1:
 -- we want to calculate the avergae salary per department and then find all employees whose salary is above the average salry of thie department.
+ with avg_sal as
+  (select dept,
+          avg(salary) as avg_salary
+   from employees
+   group by dept)
+select e.emp_id,
+       e.fname,
+       e.dept,
+       e.salary,
+       a.avg_salary
+from employees e
+join avg_sal a on e.dept=a.dept
+where e.salary>a.avg_salary;
+
+emp_id | fname | dept | salary | avg_salary --------+--------+-----------+----------+--------------------
+ 5 | Kavita | HR | 47000.00 | 46000.000000000000 6 | Amit | Marketing | 52000.00 | 51000.000000000000 9 | Anjali | Finance | 61000.00 | 60500.000000000000 3 | Arjun | IT | 71000.00 | 55500.000000000000;
