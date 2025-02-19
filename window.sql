ac44abe840d4606650b87740185d9f80a96cb833
somyagupta05 <somyagupta052003@gmail.com>
2025-02-19 08:33:27 +0530

    windows

--- window.sql
+++ window.sql
@@ -1,2 +1,7 @@
 -- window functions, also known as analytic functions allow you to perform calculation across a set of rows related to the current row.
 -- Defined by an OVER() clause.
+ -- it will only display total salary but we want other rows too so--
+
+select sum(salary)
+from employees;
+
