efa9329bb3f103ab2c5ca6dbca16508a91e2ba6c
somyagupta05 <somyagupta052003@gmail.com>
2025-02-19 08:41:28 +0530

    windows benefits

--- window.sql
+++ window.sql
@@ -14,7 +14,15 @@ from employees;
 
 -- running sum-sum is incresing in each row as new value of each row is being added
 fname | salary | sum --------+----------+-----------
- Priya | 45000.00 | 45000.00 Kavita | 47000.00 | 92000.00 Neha | 48000.00 | 140000.00 Raj | 50000.00 | 240000.00 Vijay | 50000.00 | 240000.00 Amit | 52000.00 | 292000.00 Rahul | 53000.00 | 345000.00 Suman | 60000.00 | 405000.00 Anjali | 61000.00 | 466000.00 Arjun | 71000.00 | 537000.00 (10 rows) --  BENEFITS OF WINDOW FUNCTIONS
+ Priya | 45000.00 | 45000.00 Kavita | 47000.00 | 92000.00 Neha | 48000.00 | 140000.00 Raj | 50000.00 | 240000.00 Vijay | 50000.00 | 240000.00 Amit | 52000.00 | 292000.00 Rahul | 53000.00 | 345000.00 Suman | 60000.00 | 405000.00 Anjali | 61000.00 | 466000.00 Arjun | 71000.00 | 537000.00 (10 rows)--  BENEFITS OF WINDOW FUNCTIONS
 -- 1. ADVANCED ANALYTICS: They enable complex calculations like running totals, moving averages,rank calculations, and cumulative distributions.
 -- 2.Non-Aggregating: unlike aggregate functions, window function do not collapse rows. This means you can calcualte aggregated while retaining individual row details.
 -- 3. Flexibility: They can be used in various clauses of sql, such as SELECT,ORDER BY, and HAVING providing a lot of flexibility in writting querries.
+ -- example...........................
+ -- moving average
+
+select fname,
+       salary,
+       avg(salary) over(
+                        order by salary)
+from employees;
